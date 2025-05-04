48a8ebd970a85e71f139ff21e9034358c3cd64b0
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-04 21:18:50 +0600

    Express js install & setup

--- index copy.js
+++ index copy.js
@@ -17,3 +17,52 @@ l.forEach((value, index) => {
 
 console.log(addToCart()); // This will output "Add to Cart"
 console.log(changeQty()); // This will output 5
+
+
+// node js programming language program
+
+let http = require("http");
+
+let server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    // http://localhost:8000
+    res.end("Welcome, Hello World!");
+  }
+
+  if (req.url === "/about") {
+    // http://localhost:8000/about
+    res.end("About Us, Hello World!");
+  }
+
+  if (req.url === "/contact") {
+    // http://localhost:8000/contact
+    res.end("Contact Us, Hello World!");
+  }
+
+  if (req.url === "/news") {
+    // http://localhost:8000/news
+    let obj = {
+      status: 1,
+      data: [
+        {
+          newsTitle: "News Title 1",
+          newsDescription: "News Description 1",
+          newsDate: "2023-10-01",
+        },
+        {
+          newsTitle: "News Title 2",
+          newsDescription: "News Description 2",
+          newsDate: "2023-10-02",
+        },
+        {
+          newsTitle: "News Title 3",
+          newsDescription: "News Description 3",
+          newsDate: "2023-10-03",
+        },
+      ],
+    };
+    res.end(JSON.stringify(obj)); // Convert the object to a JSON string
+  }
+});
+
+server.listen("8000"); // http://localhost:8000
--- index.js
+++ index.js
@@ -1,45 +1,9 @@
-let http = require("http");
+let express = require("express");
 
-let server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    // http://localhost:8000
-    res.end("Welcome, Hello World!");
-  }
+let app = express();
 
-  if (req.url === "/about") {
-    // http://localhost:8000/about
-    res.end("About Us, Hello World!");
-  }
-
-  if (req.url === "/contact") {
-    // http://localhost:8000/contact
-    res.end("Contact Us, Hello World!");
-  }
-
-  if (req.url === "/news") {
-    // http://localhost:8000/news
-    let obj = {
-      status: 1,
-      data: [
-        {
-          newsTitle: "News Title 1",
-          newsDescription: "News Description 1",
-          newsDate: "2023-10-01",
-        },
-        {
-          newsTitle: "News Title 2",
-          newsDescription: "News Description 2",
-          newsDate: "2023-10-02",
-        },
-        {
-          newsTitle: "News Title 3",
-          newsDescription: "News Description 3",
-          newsDate: "2023-10-03",
-        },
-      ],
-    };
-    res.end(JSON.stringify(obj)); // Convert the object to a JSON string
-  }
+app.get("/", (req, res) => {
+  res.send({ status: 1, msg: "Home Page API" });
 });
 
-server.listen("8000"); // http://localhost:8000
+app.listen("8000");
