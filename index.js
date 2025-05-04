a75ca9f70dbeccc8a4bbbb79984e67e4df42b318
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-04 18:01:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Welcome to the JavaScript world!");
+console.log("This is a simple JavaScript program.");
+console.log(10 + 20); // This will output 30
+console.log("The sum of 10 and 20 is: " + (10 + 20));
+console.log("This is a string with a number: " + 42);
+
