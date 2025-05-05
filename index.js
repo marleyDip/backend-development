d106366b54a412e8b1bf9883b79c8bc425719551
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-05 13:44:09 +0600

    App level  middleware

--- index.js
+++ index.js
@@ -3,16 +3,61 @@ let express = require("express");
 let app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 
+let myToken = "12345"; // Example token for validation
+
+let myPass = "12345"; // Example token for validation
+
 let checkToken = (req, res, next) => {
+  console.log(req.query.token); // Log the token from the query string
+  if (req.query.token === "" || req.query.token === undefined) {
+    return res.send({
+      status: 0,
+      msg: "Token is required",
+    });
+  }
+
+  if (req.query.token != myToken) {
+    return res.send({
+      status: 0,
+      msg: "Token is invalid",
+    });
+  }
+
+  next();
+}; // Middleware function to check token
+
+/* let checkToken = (req, res, next) => {
   console.log("Middleware: Checking token...");
   next(); // Call next() to pass control to the next middleware or route handler
   // You can add your token validation logic here
-};
+}; */
 
 app.use(checkToken); // Use the middleware for all routes
 
+app.use((req, res, next) => {
+  if (req.query.pass === "" || req.query.pass === undefined) {
+    return res.send({
+      status: 0,
+      msg: "Pass is required",
+    });
+  }
+
+  if (req.query.pass != myPass) {
+    return res.send({
+      status: 0,
+      msg: "Pass is invalid",
+    });
+  }
+
+  next();
+}); // Middleware to log request details
+
 app.get("/", (req, res) => {
   res.send({ status: 1, msg: "Home Page API" });
+});
+
+app.get("/news", (req, res) => {
+  res.send({ status: 1, msg: "News Page API" });
 });
 
 app.get("/news/:id", (req, res) => {
